9ce81116cc9db8825b01bbc7a26c3f93a69605d4
Proustibat <jennifer.proust@gmail.com>
2017-11-20 12:42:45 +0100

    feat(Group Element (D3 part.)): Each type of transformations with D3

--- src/pages/group-element-d3/group-element-d3.ts
+++ src/pages/group-element-d3/group-element-d3.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 import * as d3 from 'd3-selection';
 
 @Component({
@@ -7,18 +7,16 @@ import * as d3 from 'd3-selection';
 })
 export class GroupElementD3Page {
 
-    constructor() {}
+    constructor(private elementRef:ElementRef) {}
 
     ionViewDidLoad() {
-
-        this.createSVG( '#svg-element' );
-
-        this.createSVG( '#svg-element-transform', true );
-
+        this.elementRef.nativeElement.querySelectorAll('.svg-element').forEach( container => {
+            this.createSVG('#' + container.id, JSON.parse(container.getAttribute('data-transformations')));
+        });
     }
 
 
-    createSVG( wrapperId:string, transformCircles:boolean = false ): void {
+    createSVG( wrapperId:string, transformations?:[{group:string, properties:Array<string>}] ): void {
         let circleData = [
             { 'cx': 20, 'cy': 20, 'radius': 20, 'color' : 'green' },
             { 'cx': 70, 'cy': 70, 'radius': 20, 'color' : 'purple' }];
@@ -27,13 +25,14 @@ export class GroupElementD3Page {
             { 'rx': 110, 'ry': 110, 'height': 30, 'width': 30, 'color' : 'blue' },
             { 'rx': 160, 'ry': 160, 'height': 30, 'width': 30, 'color' : 'red' }];
 
-        let svgContainer = d3.select(wrapperId).append('svg')
+        let svgContainer = d3.select( wrapperId ).append( 'svg' )
             .attr( 'width',200 )
             .attr( 'height',200 );
 
 
         // Appending an SVG Group Element to the already defined SVG Container
         let circleGroup = svgContainer.append( 'g' );
+        let rectangleGroup = svgContainer.append( 'g' );
 
         // Add circles to the group container
         let circles = circleGroup.selectAll( 'circle' )
@@ -41,19 +40,15 @@ export class GroupElementD3Page {
             .enter()
             .append( 'circle' );
 
-        // Transform the group
-        if( transformCircles ) {
-            // TODO : more example of tranform (matrix, rotate, skew, etc)
-            circleGroup.attr( 'transform', 'translate( 80, 0 )' );
-        }
-
         circles
             .attr( 'cx', d => d.cx )
             .attr( 'cy', d => d.cy )
             .attr( 'r', d => d.radius )
             .style( 'fill', d => d.color );
 
-        let rectangles = svgContainer.selectAll( 'rect' )
+
+        // Add rectangles to the group container
+        let rectangles = rectangleGroup.selectAll( 'rect' )
             .data( rectangleData )
             .enter()
             .append( 'rect' );
@@ -64,6 +59,27 @@ export class GroupElementD3Page {
             .attr( 'height', d => d.height )
             .attr( 'width', d => d.width )
             .style( 'fill', d => d.color );
+
+
+        // Transform the groups
+        if( transformations ) {
+            transformations.forEach( transformation => {
+                let group = null;
+                switch ( transformation.group ) {
+                    case 'circles':
+                        group = circleGroup;
+                        break;
+                    case 'rectangles':
+                        group = rectangleGroup;
+                        break;
+                }
+                if( group ) {
+                    transformation.properties.forEach( property => {
+                        group.attr( 'transform', property );
+                    });
+                }
+            });
+        }
 
     }
 
