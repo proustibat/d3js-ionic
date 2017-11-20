bea4a7c2ff951d99380282f28bc607e182a98ff7
Proustibat <jennifer.proust@gmail.com>
2017-11-20 12:58:43 +0100

    feat(Basic SVG Elements): Add block code of tutorial and comments

--- src/pages/basic-elements/basic-elements.ts
+++ src/pages/basic-elements/basic-elements.ts
@@ -17,21 +17,34 @@ export class BasicElementsPage {
 
     createCircles():void {
 
+        // the data set
         let circleRadii = [ 40, 20, 10 ];
 
+        // an SVG element to hold the SVG Circles
         let svgContainer = d3.select( '#circles' ).append( 'svg' )
             .attr( 'width', 300 )
             .attr( 'height', 100 )
             .style( 'border', '1px solid black' );
 
-        let circles = svgContainer.selectAll( 'circle' )
+        let circles = svgContainer
+            // selectAll circle
+            .selectAll( 'circle' )
+            // bind the data
             .data( circleRadii )
+            // select the virtual .enter() selection
             .enter()
+            // append the SVG circle elements
             .append( 'circle' );
 
-        circles.attr( 'cx', 50 )
+        // Specify the attributes for each circle.
+        circles
+            // cx - The x-axis coordinate of the center of the circle
+            .attr( 'cx', 50 )
+            // cy - The y-axis coordinate of the center of the circle
             .attr( 'cy', 50 )
+            // r - The radius of the circle.
             .attr( 'r',  d => d )
+            // use D3.js' .style() operator to modify the CSS style property
             .style( 'fill', d => {
                 let returnColor;
                 if ( d === 40 ) {
@@ -59,6 +72,9 @@ export class BasicElementsPage {
             .append( 'circle' );
 
         circles
+            // add the attributes to the circle selection.
+            // The data set specifies the circle center's x-coordinate and y-coordinate,
+            // use D3.js to update the "cx" and "cy" of the specified data above
             .attr( 'cx', d => d )
             .attr( 'cy', d => d )
             .attr( 'r', 20 )
