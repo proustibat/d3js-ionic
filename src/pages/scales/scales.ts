65dbe5eb85df5ed145dfe845086491767e47ec51
Proustibat <jennifer.proust@gmail.com>
2017-11-20 16:26:53 +0100

    feat(D3.js Scales): Add blocks of code for tutorial and comments

--- src/pages/scales/scales.ts
+++ src/pages/scales/scales.ts
@@ -25,16 +25,19 @@ export class ScalesPage {
         this.initialDataToScale = [ 0, 1000, 3000, 2000, 5000, 4000, 7000, 6000, 9000, 8000, 10000 ];
 
         let linearScale = d3Scale
+            // Constructs a new continuous linear scale with
+            // the unit domain [0, 1], the unit range [0, 1],
+            // the default interpolator and clamping disabled
             .scaleLinear()
+            // Sets the scale’s domain to the specified array of numbers.
             .domain([
                 d3Array.min( this.initialDataToScale ),
-                d3Array.max( this.initialDataToScale)
+                d3Array.max( this.initialDataToScale )
             ])
+            // Sets the scale’s range to the specified array of values
             .range( [ 0, 100 ] );
 
-        this.initialDataToScale.forEach( data =>
-            this.scaledData.push( linearScale( data ) )
-        );
+        this.scaledData = this.initialDataToScale.map( data => linearScale( data ) );
 
     }
 
