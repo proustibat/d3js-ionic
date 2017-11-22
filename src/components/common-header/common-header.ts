293a90dce8c0b5b7aac4d6cf42005f57ee3c3ee7
Proustibat <jennifer.proust@gmail.com>
2017-11-22 18:04:43 +0100

    fix(Header): Common hear customizable

--- src/components/common-header/common-header.ts
+++ src/components/common-header/common-header.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'common-header',
-  templateUrl: 'common-header.html'
+    selector: 'common-header',
+    templateUrl: 'common-header.html'
 })
 export class CommonHeaderComponent {
 
-  constructor() {}
+    @ViewChild('ref') refIonTitle;
+
+    defaultTitle:string = "D3.js with Ionic";
+
+    constructor(private renderer:Renderer2) {
+
+    }
+    ngAfterViewInit() {
+        let titleEl = this.refIonTitle.getElementRef().nativeElement.querySelector('.toolbar-title');
+        if( titleEl.childNodes.length === 0 ) {
+            this.renderer.appendChild(titleEl, this.renderer.createText(this.defaultTitle));
+        }
+
+    }
 
 }
